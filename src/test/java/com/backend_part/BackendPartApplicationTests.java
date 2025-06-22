3b453ac6882d644f7c9b9c01052e76d7bd31633d
SysoievB <sysoiev.bogdan.ua@gmail.com>
2025-06-22 12:28:11 +0200

    Init commit

---
+++ src/test/java/com/backend_part/BackendPartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend_part;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendPartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
